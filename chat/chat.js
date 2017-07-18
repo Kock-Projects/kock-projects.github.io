5ec12c2dbbba3ce28a998c44c21aa57e90c458f1
bulldozer <bulldozer549@gmail.com>
2017-07-18 22:50:50 +0200

    EOL update 4
    + Added fullscreen for chat
    + Added favicon
    * minor CSS changes
    * images now link to this repo

--- chat/chat.js
+++ chat/chat.js
@@ -1,4 +1,30 @@
-maximized = false;
+var maximized = false;
+var fs = false;
+function rfs(state) {
+	if (state == 1) {
+		if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {
+			if (document.documentElement.requestFullscreen) {
+				document.documentElement.requestFullscreen();
+			} else if (document.documentElement.msRequestFullscreen) {
+				document.documentElement.msRequestFullscreen();
+			} else if (document.documentElement.mozRequestFullScreen) {
+				document.documentElement.mozRequestFullScreen();
+			} else if (document.documentElement.webkitRequestFullscreen) {
+				document.documentElement.webkitRequestFullscreen(document.documentElement.ALLOW_KEYBOARD_INPUT);
+			}
+		}
+	} else if (state == 0) {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.msExitFullscreen) {
+			document.msExitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitExitFullscreen) {
+			document.webkitExitFullscreen();
+		}
+	}
+}
 window.onload = function() {
 	document.getElementById("max").onclick = function() {
 		if (!maximized) {
@@ -29,17 +55,115 @@ window.onload = function() {
 			document.body.style.height = "auto";
 			document.getElementById("nav").style.margin = "auto";
 			document.getElementById("nav").style.width = "838px";
-			document.getElementById("nav").style.borderRadius = "12px 12px 0px 0px";
+			document.getElementById("nav").style.borderRadius = "8px 8px 0px 0px";
 			document.getElementById("desc").style.width = "800px";
 			document.getElementById("desc").style.padding = "10px 25px 25px 25px";
 			document.getElementById("desc").style.margin = "auto";
-			document.getElementById("desc").style.borderRadius = "0px 0px 12px 12px";
+			document.getElementById("desc").style.borderRadius = "0px 0px 8px 8px";
 			document.getElementById("desc").style.height = "auto";
 			document.getElementById("desc").style.overflow = "visible";
 			document.getElementById("chat").style.height = "400px";
 			document.getElementById("chat").style.width = "800px";
 			document.getElementById("max").innerHTML = "Maximize";
 			maximized = false;
+		}
+	}
+	document.getElementById("fs").onclick = function() {
+		if(!fs) {
+			if(maximized) {
+				document.getElementById("nav").style.display = "none";
+				document.getElementById("desc").style.height = "100%";
+				document.documentElement.style.width = "100%";
+			} else {
+				document.getElementById("logo").style.display = "none";
+				document.documentElement.style.height = "100%";
+				document.documentElement.style.width = "100%";
+				document.body.style.margin = "0";
+				document.body.style.height = "100%";
+				document.getElementById("nav").style.display = "none";
+				document.getElementById("desc").style.width = "auto";
+				document.getElementById("desc").style.padding = "0";
+				document.getElementById("desc").style.margin = "0";
+				document.getElementById("desc").style.borderRadius = "0";
+				document.getElementById("desc").style.height = "100%";
+				document.getElementById("desc").style.overflow = "hidden";
+				document.getElementById("chat").style.height = "100%";
+				document.getElementById("chat").style.width = "100%";
+			}
+			var fs2 = document.createElement("BUTTON");
+			fs2.innerHTML = "<img src=\"fsx.svg\">";
+			fs2.style.position = "absolute";
+			fs2.style.top = "0";
+			fs2.style.left = "0";
+			fs2.style.border = "0";
+			fs2.style.width = "24px";
+			fs2.style.height = "24px";
+			fs2.style.outline = "0";
+			fs2.style.padding = "0";
+			fs2.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+			fs2.style.color = "#FFF";
+			document.body.appendChild(fs2);
+			fs2.id = "fs2";
+			rfs(1);
+			fs = true;
+		}
+		document.addEventListener("fullscreenchange", function() {
+			if (!document.fullscreenElement) {
+				efs();
+			}
+		});
+		document.addEventListener("webkitfullscreenchange", function() {
+			if (!document.webkitFullscreenElement) {
+				efs();
+			}
+		});
+		document.addEventListener("mozfullscreenchange", function() {
+			if (!document.mozFullScreenElement) {
+				efs();
+			}
+		});
+		document.addEventListener("msfullscreenchange", function() {
+			if (!document.msFullscreenElement) {
+				efs();
+			}
+		});
+		document.getElementById("fs2").onclick = function() {
+			efs();
+		}
+		document.onkeydown = function(ev) {
+			ev = ev || window.event;
+			if (ev.keyCode == 27) {
+				efs();
+			}
+		}
+		function efs() {
+			if(fs) {
+				if(maximized) {
+					document.getElementById("nav").style.display = "block";
+					document.getElementById("desc").style.height = "calc(100% - 38px)";
+					document.getElementById("desc").style.height = "-webkit-calc(100% - 38px)";
+					document.getElementById("desc").style.height = "-moz-calc(100% - 38px)";
+					document.getElementById("desc").style.height = "-o-calc(100% - 38px)";
+				} else {
+					document.getElementById("logo").style.display = "inline";
+					document.documentElement.style.height = "auto";
+					document.documentElement.style.width = "auto";
+					document.body.style.margin = "8px";
+					document.body.style.height = "auto";
+					document.getElementById("nav").style.display = "block";
+					document.getElementById("desc").style.width = "800px";
+					document.getElementById("desc").style.padding = "10px 25px 25px 25px";
+					document.getElementById("desc").style.margin = "auto";
+					document.getElementById("desc").style.borderRadius = "0px 0px 8px 8px";
+					document.getElementById("desc").style.height = "auto";
+					document.getElementById("desc").style.overflow = "visible";
+					document.getElementById("chat").style.height = "400px";
+					document.getElementById("chat").style.width = "800px";
+				}
+				rfs(0);
+				document.body.removeChild(document.getElementById("fs2"));
+				fs = false;
+			}
 		}
 	}
 }
--- chat/script.js
+++ chat/script.js
@@ -7,22 +7,26 @@ function gRN(n) {
 }
 gNick = "Kock" + gRN(5);
 function ncheck(nick) {
-	nchar = /^[\w\\\[\]_{}^`|\-]*$/;
-	ichar = /[\d\-]/;
-	if (!nchar.test(nick)) {
-		alert("Invalid nickname.\nOnly these characters are allowed: a-z A-Z 0-9 _ - \ [ ] { } ^ ` |\n(numbers and hyphen-minus signs cannot be at the beginning of the nickname)");
-		return false;
-	} else if (ichar.test(nick.charAt(0))) {
-		alert("Numbers and hyphen-minus signs cannot be at the beginning of the nickname.");
-		return false;
+	if (!nick) {
+		alert("The nickname field cannot be empty.");
 	} else {
-		return true;
+		nchar = /^[\w\\\[\]_{}^`|\-]*$/;
+		ichar = /[\d\-]/;
+		if (!nchar.test(nick)) {
+			alert("Invalid nickname.\nOnly these characters are allowed: a-z A-Z 0-9 _ - \ [ ] { } ^ ` |\n(The nickname cannot start with a number or a hyphen)");
+			return false;
+		} else if (ichar.test(nick.charAt(0))) {
+			alert("The nickname cannot start with a number or a hyphen.");
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
-function kock(fs) {
+function kock(pp) {
 	nick = document.getElementById("nick").value;
-	if (fs) {
-		window.top.location.href = "gateway";
+	if (pp) {
+		window.open("gateway.html", "chat", "directories=0, location=0, menubar=0, resizable=1, status=0, toolbar=0, width=700, height=500");
 	} else {
 		if (ncheck(nick)) {
 			window.location.href = "https://qchat.rizon.net/?nick=" + nick + "&channels=Kock&saturation=-9&lightness=-86&tlightness=100";
@@ -34,20 +38,22 @@ function kock(fs) {
 }
 function fNick() {
 	document.getElementById("nick").focus();
+	document.getElementById("nick").select();
 }
 window.onload = function() {
 	iframe = window.top.location != window.self.location;
 	if (iframe) {
 		document.body.style.backgroundColor = "transparent";
-	} else {
-		document.body.removeChild(document.getElementById("confs"));
+	}
+	if (window.opener) {
+		document.body.removeChild(document.getElementById("pop"));
 	}
 	document.getElementById("nick").value = gNick;
 	fNick();
 	document.getElementById("connect").onclick = function() {
 		kock(false);
 	}
-	document.getElementById("confs").onclick = function() {
+	document.getElementById("pop").onclick = function() {
 		kock(true);
 	}
 }
